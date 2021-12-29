d1f2cc44cb5d49e2f88cf61ec8c929f1b90394c4
Glenn Pinto <gvpinto@gmail.com>
2021-12-29 14:12:46 -0500

    Part 2. Add Changes from Assignments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +11,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   projStatus = ['Stable', 'Critical', 'Finished'];
 
-  forbiddenProjNames = ['test'];
+  forbiddenProjNames = ['Test'];
 
   projForm: FormGroup;
 
   ngOnInit(): void {
     this.projForm = new FormGroup({
-      projName: new FormControl(null, [
-        Validators.required,
-        this.forbiddenNames.bind(this),
-      ]),
+      projName: new FormControl(
+        null,
+        [Validators.required, CustomValidators.invalidProjectName],
+        CustomValidators.asyncInvalidProjectName
+        // this.forbiddenNames.bind(this)
+      ),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      projStatus: new FormControl(null, Validators.required),
+      projStatus: new FormControl('Critical', Validators.required),
     });
   }
 
@@ -28,11 +32,24 @@ export class AppComponent implements OnInit {
     console.log(this.projForm);
   }
 
-  forbiddenNames(control: FormControl): { [s: string]: boolean } {
-    if (this.forbiddenProjNames.indexOf(control.value) !== -1) {
-      return { forbiddenName: true };
-    } else {
-      return null;
-    }
-  }
+  //   forbiddenNames(control: FormControl): { [s: string]: boolean } {
+  //     if (this.forbiddenProjNames.indexOf(control.value) !== -1) {
+  //       return { forbiddenName: true };
+  //     } else {
+  //       return null;
+  //     }
+  //   }
+
+  //   forbiddenNames(control: FormControl): Promise<any> | Observable<any> {
+  //     const promise = new Promise((resolve, reject) => {
+  //       setTimeout(() => {
+  //         if (this.forbiddenProjNames.indexOf(control.value) !== -1) {
+  //           resolve({ invalidProjectName: true });
+  //         } else {
+  //           resolve(null);
+  //         }
+  //       }, 2000);
+  //     });
+  //     return promise;
+  //   }
 }
